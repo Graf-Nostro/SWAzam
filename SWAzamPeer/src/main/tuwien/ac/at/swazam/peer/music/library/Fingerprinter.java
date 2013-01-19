72352192b6b4ab63e3722b96479eb4c8ec356cff
Graf-Nostro <nostro.graff@gmail.com>
2013-01-19 18:15:26 +0100

    Changed Music library to find fingerprint by fingerprint

--- SWAzamPeer/src/main/tuwien/ac/at/swazam/peer/music/library/Fingerprinter.java
+++ SWAzamPeer/src/main/tuwien/ac/at/swazam/peer/music/library/Fingerprinter.java
@@ -14,13 +14,11 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import ac.at.tuwien.infosys.swa.audio.Fingerprint;
 import ac.at.tuwien.infosys.swa.audio.FingerprintSystem;
 
-import static main.tuwien.ac.at.swazam.peer.Strings.*;
-
 /**
  * 
  * Makes a Fingerprint of a music file in the music directory/library
  * 
- * Uses lacy loading and stores the values offline on the filesystem
+ * Uses a thread to compute the fingerprints
  * 
  * @author Raunig Stefan
  */
@@ -28,9 +26,7 @@ public class Fingerprinter {
 	  
 	private static Logger logger = Logger.getLogger("main.tuwien.ac.at.swazam.peer.music.library.Fingerprinter");
 	
-	private final String path;
-	
-	private Map<String, Fingerprint> library = new HashMap<String, Fingerprint>();
+	private Map<Fingerprint, String> library = new HashMap<Fingerprint, String>();
 	private final LibrarySerializer libS = new LibrarySerializer();
 	
 	public Fingerprinter(String path){
@@ -38,19 +34,11 @@ public class Fingerprinter {
 			path = System.getProperty("user.dir")+"/library/";
 		}
 		
-		this.path = path;
+		LibraryThread th = new LibraryThread(path);
+		th.start();
 
 		//load in file
-		if(new File(path + "Library.dat").exists()) library = libS.deserializeMap();
-		
-		File[] files = listFiles(path);
-		
-		for(File f : files){
-			if( !library.containsKey(f.getName()) ) {
-				//puts a dummy obj. into map for lazy loading			
-				library.put(f.getName(), null);
-			}
-		}		
+		if(new File(path + "Library.dat").exists()) library = libS.deserializeMap();		
 	}
 	
 	/**
@@ -68,79 +56,15 @@ public class Fingerprinter {
 	}
 		
 	/**
-	 * Lists files in directory 
+	 * Search the library for a fingerprint match
 	 * 
-	 * @param path to files
-	 * @return all files in that path
+	 * @param fp
+	 * @return filename or No match found
 	 */
-	private File[] listFiles(final String path){
-		String files;
-		File folder = new File(path);
+	public String matchFingerprintToLibrary(final Fingerprint fp){		
+		logger.log(Level.INFO, "Search for file");
 		
-		File[] listOfFiles = folder.listFiles(); 	
-		
-		//print out all files
-		for (int i = 0; i < listOfFiles.length; i++){
-			if (listOfFiles[i].isFile()) {
-				files = listOfFiles[i].getName();
-				logger.log(Level.FINE, files);
-			}
-		}
-		return listOfFiles;
+		if(library.containsKey(fp)) return library.get(fp);
+		else 						return "No match found";
 	}
-
-	/**
-	 * Loads a Fingerprint of a file from the Map, uses lazy loading at this point
-	 * 
-	 * @param fileName
-	 * @return fingerprint if found, else null and throws an exception
-	 */
-	public Fingerprint loadFingerprintFromMap(final String fileName) throws NoSuchFileInMusicLibrary{
-		if(library.containsKey(fileName)) {
-			
-			Fingerprint fp = library.get(fileName);
-			
-			if(fp != null) {
-				logger.log(Level.INFO, FOUND_FILE_MAP); 
-				return fp; 
-			}
-			
-			File f = new File(path + fileName);
-			
-			try {
-				//lazy load calc. fingerprint at the time it is needed
-				Fingerprint print = Fingerprinter.getFingerprint(f);
-				library.put(fileName, print);
-				
-				logger.log(Level.FINE, library.toString());
-				
-				libS.serializeMap(library);
-				
-				return print;
-			} catch (IOException e) {
-				logger.log(Level.SEVERE, IOEXCEPTION_FINGERPRINT);
-				
-				e.printStackTrace();
-			} catch (UnsupportedAudioFileException e) {
-				logger.log(Level.SEVERE, "File causing trouble: " +  f.getName());
-				logger.log(Level.SEVERE, UNSUPPORTED_AUDIO_FILE);
-				
-				//e.printStackTrace();
-			}
-		} else {
-			throw new NoSuchFileInMusicLibrary(FILE_NOT_FOUND_LIBRARY);
-		}
-		//this code is never reached
-		return null;
-	}
-
-	public String matchFingerprintToLibrary(final Fingerprint fp){
-		/**
-		 * DEGUB MOCKUP 
-		 */
-		
-		if( library.containsValue(fp) ) return "YES";
-		else return "NO";
-	}
-
 }
