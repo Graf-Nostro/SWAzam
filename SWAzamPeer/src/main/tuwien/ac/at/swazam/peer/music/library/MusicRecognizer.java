041080a9b2f8a83b0651894190755f27ae11a7bb
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-01-20 20:29:40 +0100

    Added MusicRecognizerTest and fixed request redirection

--- SWAzamPeer/src/main/tuwien/ac/at/swazam/peer/music/library/MusicRecognizer.java
+++ SWAzamPeer/src/main/tuwien/ac/at/swazam/peer/music/library/MusicRecognizer.java
@@ -1,6 +1,12 @@
 package main.tuwien.ac.at.swazam.peer.music.library;
 
+import main.tuwien.ac.at.swazam.peer.MainPeer;
+import main.tuwien.ac.at.swazam.peer.connector.PeerConnector;
+import main.tuwien.ac.at.swazam.peer.connector.ServerConnector;
 import main.tuwien.ac.at.swazam.peer.util.Peer;
+import main.tuwien.ac.at.swazam.peer.util.PeerFinder;
+import main.tuwien.ac.at.swazam.peer.util.PeerRegistry;
+import main.tuwien.ac.at.swazam.util.PropertyReader;
 import ac.at.tuwien.infosys.swa.audio.Fingerprint;
 
 /**
@@ -12,16 +18,34 @@ public class MusicRecognizer
 {
 	private Peer peer;
 	private Fingerprint fingerprint;
+	private PeerFinder peerFinder;
 	
 	public MusicRecognizer(Peer peer, Fingerprint fingerprint) {
 		this.peer = peer;
 		this.fingerprint = fingerprint;
 		
+		String serverUrl = PropertyReader.getInstance(MainPeer.PROPERTY_FILE).getProperty("server-url");
+		if (null == serverUrl) {
+			serverUrl = System.getProperty("server-url");
+		}
+		
+		peerFinder = new PeerFinder(new PeerRegistry(
+				ServerConnector.getInstance().setServerURL(serverUrl)
+		).updatePeersFromServer());
 		new LibrarySerializer(peer.getLibrary()).deserialize();
 	}
 	
 	public String recognize() {
 		String result = peer.getLibrary().matchFingerprint(fingerprint);
-		return result;
+		if (null == result) {
+			Peer peer = peerFinder.findPeer();
+			if (peer == null) {
+				return null;
+			} else {
+				return PeerConnector.getInstance().redirectMatchRequest(peer, fingerprint);
+			}
+		} else {
+			return result;
+		}
 	}
 }
---
+++ SWAzamPeer/src/test/tuwien/ac/at/swazam/music/library/MusicRecognizerTest.java
@@ -0,0 +1,77 @@
+package test.tuwien.ac.at.swazam.music.library;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import main.tuwien.ac.at.swazam.peer.MainPeer;
+import main.tuwien.ac.at.swazam.peer.music.library.Fingerprinter;
+import main.tuwien.ac.at.swazam.peer.music.library.MusicRecognizer;
+import main.tuwien.ac.at.swazam.peer.util.Peer;
+import main.tuwien.ac.at.swazam.util.PropertyReader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ac.at.tuwien.infosys.swa.audio.Fingerprint;
+
+/**
+ * MusicRecognizerTest
+ * 
+ * @author Florian Eckerstorfer <florian@eckerstorfer.co>
+ */
+public class MusicRecognizerTest
+{
+	private final String FILE1 = PropertyReader.getInstance(MainPeer.PROPERTY_FILE).getProperty("library-directory") + "/f01.wav";
+	private final String FILE2 = PropertyReader.getInstance(MainPeer.PROPERTY_FILE).getProperty("library-directory") + "/d01.wav";
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+	
+	@Test
+	public void testRecognize() {
+		Peer peer = new Peer("florian", "localhost", 8080);
+		Fingerprint fingerprint = null;
+		try {
+			fingerprint = Fingerprinter.getFingerprint(new File(FILE1));
+			assertTrue(true);
+		} catch (IOException e) {
+			assertTrue(false);
+		} catch (UnsupportedAudioFileException e) {
+			assertTrue(false);
+		}
+		
+		MusicRecognizer musicRecognizer = new MusicRecognizer(peer, fingerprint);
+		String result = musicRecognizer.recognize();
+		assertEquals("f01.wav", result);
+	}
+	
+	@Test
+	public void testRecognizeWithRedirect() {
+		Peer peer = new Peer("florian", "localhost", 8080);
+		Fingerprint fingerprint = null;
+		try {
+			fingerprint = Fingerprinter.getFingerprint(new File(FILE2));
+			assertTrue(true);
+		} catch (IOException e) {
+			assertTrue(false);
+		} catch (UnsupportedAudioFileException e) {
+			assertTrue(false);
+		}
+		
+		MusicRecognizer musicRecognizer = new MusicRecognizer(peer, fingerprint);
+		String result = musicRecognizer.recognize();
+		assertEquals("d01.wav", result);
+	}
+}
